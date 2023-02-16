70329155e9fdf1089470a0608ddcececae4ef2f9
ChangYu Huang <jack.mainwork@gmail.com>
2023-02-16 11:47:04 +0800

    Deploy version

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,4 +1,3 @@
-from .image import image_bp
 from .bot import bot_bp
 import json
 
@@ -6,5 +5,4 @@ with open("setting.json") as json_file:
     config = json.load(json_file)
 
 def init_app(app):
-    app.register_blueprint(image_bp)
     app.register_blueprint(bot_bp)
