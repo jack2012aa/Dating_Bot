a2d2221bdc36a29fbe2d81dc0b4c5dd007f01f01
ChangYu Huang <jack.mainwork@gmail.com>
2023-02-17 22:11:41 +0800

    Fix wrong if else statement.

--- test_model.py
+++ test_model.py
@@ -1,3 +1,3 @@
 import models
 
-print(models.book.get_books("U32049c61c2c040c599e852ba4e1b181f", '女', '男', ['傳記'], []))
+print(models.book.get_exchanged_book_and_lineID("U32049c61c2c040c599e852ba4e1b181f"))
