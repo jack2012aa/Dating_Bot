a554abff6341b4253765d8d30006feb711b5f29a
ChangYu Huang <jack.mainwork@gmail.com>
2023-02-19 21:00:23 +0800

    Add user contract.

--- services/user/add_new_user.py
+++ services/user/add_new_user.py
@@ -5,5 +5,5 @@ from linebot.models import Event, TextSendMessage
 def add_new_user(event: Event):
     '''Insert userID into friends table and show welcome message'''    
     models.user.insert_user(event.source.user_id)
-    line_bot_api.reply_message(event.reply_token, TextSendMessage(text = text_dict["Welcome"]))
+    line_bot_api.reply_message(event.reply_token, [TextSendMessage(text = text_dict["Welcome"]), TextSendMessage(text = text_dict["User contract"])])
     return 0
